c31fdaeae03107ecb3c1c38a27681030557362d5
dhruvak690 <dhruvak690@gmail.com>
2024-03-30 12:35:01 +0530

    configuration of security for project using jsonWebtoken

---
+++ src/main/java/com/dhruva/config/jwtConstant.java
@@ -0,0 +1,6 @@
+package com.dhruva.config;
+
+public class jwtConstant {
+    public static final String SECRET_KEY = "qwerty ytrewq";
+    public static final String JWT_HEADER = "Authorization";
+}
